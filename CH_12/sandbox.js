e04f053336b53cb60e53a21bbc9d56c6eb318b04
Matt <mattppalmer@gmail.com>
2019-10-11 16:30:33 -0400

    chapter 12 done

---
+++ CH_12/sandbox.js
@@ -0,0 +1,64 @@
+// http requests
+
+// const getTodos = resource => {
+
+//     return new Promise((resolve, reject) => {
+//         const request = new XMLHttpRequest();
+
+//         request.addEventListener('readystatechange', () => {
+//             if(request.readyState === 4 && request.status === 200){
+//                 const data = JSON.parse(request.responseText);
+//                 resolve(data);
+//             } else if(request.readyState === 4){
+//                 reject('error getting resource');
+//             }
+//         });
+        
+//         request.open('GET', resource);
+//         request.send();
+    
+//     });   
+//     };
+    
+
+// getTodos('todos/matt.json').then(data => {
+//     console.log('promise resolved:', data);
+//     return getTodos('todos/jasmine.json')
+// }).then(data => {
+//     console.log('promise 2 resolved:', data)
+// }).catch(err => {
+//     console.log('promise rejected:', err);
+// });
+
+
+// fetch api 
+
+// fetch('todos/matt.json').then(response => {
+//     console.log('resolved', response);
+//     return response.json();
+// }).then(data => {
+//     console.log(data)
+// }).catch(err => {
+//     console.log('rejected', err)
+// });
+
+// async & await
+
+const getTodos = async () => {
+
+    const response = await fetch('todos/matt.json');
+
+    if(response.status !== 200){
+        throw new Error('cannot fetch data');
+    }
+
+    const data = await response.json();
+    return data;
+
+};
+
+
+getTodos()
+    .then(data => console.log('resolved:', data))
+    .catch(err => console.log('rejected:', err));
+
