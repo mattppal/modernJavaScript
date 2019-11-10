ff616e3f689bcd7380db727c914d51d9427bbdc3
Matt <mattppalmer@gmail.com>
2019-11-10 14:43:30 -0500

    ch 17 done

--- CH_17 (chat)/scripts/app.js
+++ CH_17 (chat)/scripts/app.js
@@ -3,6 +3,7 @@ const chatList = document.querySelector('.chat-list');
 const newChatForm = document.querySelector('.new-chat');
 const newNameForm = document.querySelector('.new-name');
 const updateMsg = document.querySelector('.update-msg');
+const rooms = document.querySelector('.chat-rooms');
 
 // add a new chat
 newChatForm.addEventListener('submit', e => {
@@ -26,6 +27,14 @@ newNameForm.addEventListener('submit', e => {
     document.getElementById('name').placeholder = newName
     setTimeout(() => updateMsg.innerText = '', 3000);
 });
+// update the chat room
+rooms.addEventListener('click', e => {
+    if(e.target.tagName === 'BUTTON'){
+        chatUI.clear();
+        chatroom.updateRoom(e.target.getAttribute('id'));
+        chatroom.getChats(chat => chatUI.render(chat));
+    };
+})
 
 // check local storage for a name
 const username = localStorage.username ? localStorage.username : 'anon';
