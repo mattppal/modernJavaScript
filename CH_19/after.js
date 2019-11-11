3725bc0edcffd1f9a35858d7b3fecde9287fff4a
Matt <mattppalmer@gmail.com>
2019-11-11 13:37:45 -0500

    git ignore

---
+++ CH_19/after.js
@@ -0,0 +1,9 @@
+"use strict";
+
+var name = 'jasmine';
+
+var greet = function greet(name) {
+  console.log("hello ".concat(name));
+};
+
+greet(name);
---
+++ CH_19/before.js
@@ -0,0 +1,7 @@
+const name = 'jasmine'
+
+const greet = (name) => {
+    console.log(`hello ${name}`);
+};
+
+greet(name);
